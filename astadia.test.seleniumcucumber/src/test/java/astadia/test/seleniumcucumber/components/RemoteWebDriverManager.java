da73522c05a171df37be10cdd1a96933bd17fd28
Vinod Ramakrishnan <vinod.ramakrishnan@astadia.com>
2017-10-04 20:59:20 -0500

    opera issue fixed

--- astadia.test.seleniumcucumber/src/test/java/astadia/test/seleniumcucumber/components/RemoteWebDriverManager.java
+++ astadia.test.seleniumcucumber/src/test/java/astadia/test/seleniumcucumber/components/RemoteWebDriverManager.java
@@ -21,18 +21,25 @@ import org.openqa.selenium.opera.*;
 public class RemoteWebDriverManager {
 	
 	public static final String DRIVERPATH_FIREFOX_PROPERTY_KEY="webdriver.gecko.driver";
-	public static final String DRIVERPATH_CHROME_PROPERTY_KEY="webdriver.chrome.driver";
-	public static final String DRIVERPATH_OPERA_PROPERTY_KEY="webdriver.opera.driver";
+	public static final String DRIVERPATH_CHROME_PROPERTY_KEY="webdriver.chrome.driver";	
 	public static final String DRIVERPATH_IEXPLORER_PROPERTY_KEY="webdriver.ie.driver";
 	public static final String DRIVERPATH_EDGE_PROPERTY_KEY="webdriver.edge.driver";
 	
 	
 	
 	public static final String DRIVERPATH_FIREFOX_PROPERTY_VAL="F:\\mybrowsers\\firefoxgheko\\geckodriver.exe";
-	public static final String DRIVERPATH_CHROME_PROPERTY_VAL="F:\\mybrowsers\\chrome\\chromedriver.exe";
-	public static final String DRIVERPATH_OPERA_PROPERTY_VAL="F:\\mybrowsers\\oepra\\operadriver.exe";	
+	public static final String DRIVERPATH_CHROME_PROPERTY_VAL="F:\\mybrowsers\\chrome\\chromedriver.exe";	
 	public static final String DRIVERPATH_IEXPLORER_PROPERTY_VAL="F:\\mybrowsers\\ie\\IEDriverServer.exe";
 	public static final String DRIVERPATH_EDGE_PROPERTY_VAL="F:\\mybrowsers\\edge\\MicrosoftWebDriver.exe";
+	
+	public static final String DRIVERPATH_OPERA_PROPERTY_KEY="webdriver.opera.driver";	
+	public static final String DRIVERPATH_OPERA_PROPERTY_VAL="F:\\mybrowsers\\opera1\\operadriver.exe";	
+	public static final String EXEPATH_OPERA_PROPERTY_VAL="C:\\Program Files\\Opera\\48.0.2685.35\\opera.exe";	
+	
+
+	
+	
+	
 	
 	public static ChromeOptions getChromeOptions() {
 		 ChromeOptions  options = new ChromeOptions();
@@ -54,9 +61,10 @@ public class RemoteWebDriverManager {
 		System.setProperty(DRIVERPATH_IEXPLORER_PROPERTY_KEY, DRIVERPATH_IEXPLORER_PROPERTY_VAL);
 		return options;
 	}
-	public static OperaOptions getOperaOptions() {
-		OperaOptions  options = new OperaOptions();
+	public static OperaOptions getOperaOptions() {		
 		System.setProperty(DRIVERPATH_OPERA_PROPERTY_KEY, DRIVERPATH_OPERA_PROPERTY_VAL);
+		OperaOptions  options = new OperaOptions();
+		options.setBinary(EXEPATH_OPERA_PROPERTY_VAL);
 		return options;
 	}
 }
