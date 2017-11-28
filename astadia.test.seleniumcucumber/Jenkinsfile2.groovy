734ce5d92447e73ffe903f176fd7369af186a146
Vinod Ramakrishnan <vinod.ramakrishnan@hotmail.com>
2017-11-28 11:29:40 -0600

    Add files via upload
    test

--- astadia.test.seleniumcucumber/Jenkinsfile2.groovy
+++ astadia.test.seleniumcucumber/Jenkinsfile2.groovy
@@ -4,25 +4,20 @@ def createPipeline(projectName) {
 	def ENV_GITBRANCH = "${env.CUR_GITBRANCH}";
 	def ENV_GITURL = "${env.CUR_GITURL}";
 	def ENV_GITCREDID = "${env.CUR_GITCREDID}";
-	println("The BRANCH= ${env.CUR_GITBRANCH}")
-	stages{
-		stage('Clean and Checkout'){
-			steps{				
-				cleanWs();	
-				checkout([
-					$class: 'GitSCM', 
-					branches: [[name: '*/'+ env.CUR_GITBRANCH]], 
-					doGenerateSubmoduleConfigurations: false, 
-					extensions: [], submoduleCfg: [], 
-					userRemoteConfigs: [[
-						credentialsId: '' + env.CUR_GITCREDID, 
-						url: '' + env.CUR_GITURL
-					]]
-				])
-				println("The BRANCH2= $ENV_GITBRANCH")
-			}
-		}
-	}
+	println("The BRANCH= ${env.CUR_GITBRANCH}")	
+	stage('Clean and Checkout')						
+		cleanWs();	
+		checkout([
+			$class: 'GitSCM', 
+			branches: [[name: '*/'+ env.CUR_GITBRANCH]], 
+			doGenerateSubmoduleConfigurations: false, 
+			extensions: [], submoduleCfg: [], 
+			userRemoteConfigs: [[
+				credentialsId: '' + env.CUR_GITCREDID, 
+				url: '' + env.CUR_GITURL
+			]]
+		])
+		println("The BRANCH2= $ENV_GITBRANCH")	
 }
 return this
 
