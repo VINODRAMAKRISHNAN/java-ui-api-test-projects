d4ac7a478f3c3353f34241e2b3fb60d078bbd02f
Vinod Ramakrishnan <vinod.ramakrishnan@hotmail.com>
2017-11-27 20:37:36 -0600

    Update Jenkinsfile1.groovy

--- astadia.test.seleniumcucumber/Jenkinsfile1.groovy
+++ astadia.test.seleniumcucumber/Jenkinsfile1.groovy
@@ -14,15 +14,15 @@ pipeline{
 				cleanWs();
 				checkout([
 					$class: 'GitSCM', 
-					branches: [[name: '*/' + env.CUR_BRANCH]], 
+					branches: [[name: '*/'+ env.ENV_BRANCH]], 
 					doGenerateSubmoduleConfigurations: false, 
 					extensions: [], 
 					submoduleCfg: [], 
 					userRemoteConfigs: [[
-						credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5' , 
+						credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', 
 						url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git'
 					]]
-				]);
+				])
 				//checkout([$class: 'GitSCM', branches: [[name: '*/'+ env.ENV_NAME]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git']]])
 				//checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git']]])
 				print "BROWSER-TYPE111ww"
