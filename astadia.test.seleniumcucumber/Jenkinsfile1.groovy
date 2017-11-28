f04e064fe091b7a23ac27e12828700a0be110862
Vinod Ramakrishnan <vinod.ramakrishnan@hotmail.com>
2017-11-27 19:50:28 -0600

    Update Jenkinsfile1.groovy

--- astadia.test.seleniumcucumber/Jenkinsfile1.groovy
+++ astadia.test.seleniumcucumber/Jenkinsfile1.groovy
@@ -2,7 +2,6 @@ pipeline{
 	agent {node {label "TEST-VINOD"} }
 	environment {
             ENV_NAME = "${env.CUR_BRANCH}";
-	    //ENV_CHOUT = env.MY_CHECKOUT;
 	}
 	options {
     	skipDefaultCheckout true
@@ -10,12 +9,9 @@ pipeline{
 	stages{
 		stage('Clean and Checkout'){
 			steps{
-				print "MY_CHECKOUTSTRING = ${env.ENV_NAME}"
 				print "CURRENT-BRANCH = ${env.ENV_NAME}"
 			   	print "BROWSER-TYPE-VINOD"
 				cleanWs();
-				
-				
 				checkout([$class: 'GitSCM', branches: [[name: '*/'+ env.ENV_NAME]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git']]])
 				//checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git']]])
 				print "BROWSER-TYPE111ww"
