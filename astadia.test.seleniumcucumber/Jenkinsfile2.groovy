4d8c67e5fe75a55cb9f72767af8d806e090e7dd7
Vinod Ramakrishnan <vinod.ramakrishnan@hotmail.com>
2017-11-28 11:07:54 -0600

    Add files via upload
    TEST

--- astadia.test.seleniumcucumber/Jenkinsfile2.groovy
+++ astadia.test.seleniumcucumber/Jenkinsfile2.groovy
@@ -2,11 +2,11 @@ def createPipeline(projectName) {
 	println("The projectname=" + projectName )
     println("exampleMethod")
 	environment {
-        	ENV_GITBRANCH = "${env.CUR_GITBRANCH}";
-	    	ENV_GITURL = "${env.CUR_GITURL}";
-	    	ENV_GITCREDID = "${env.CUR_GITCREDID}";
+        ENV_GITBRANCH = "${env.CUR_GITBRANCH}";
+	    ENV_GITURL = "${env.CUR_GITURL}";
+	    ENV_GITCREDID = "${env.CUR_GITCREDID}";
 	}
-	println("The projectname2=" + projectName )
+	println("The BRANCH=" + ENV_GITBRANCH )
 }
 return this
 
