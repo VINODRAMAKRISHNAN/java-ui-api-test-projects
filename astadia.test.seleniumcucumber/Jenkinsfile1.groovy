e80d8c3eddb0f9a763253d0b86f2b845b4e6536d
Vinod Ramakrishnan <vinod.ramakrishnan@hotmail.com>
2017-11-27 19:33:22 -0600

    Update Jenkinsfile1.groovy

--- astadia.test.seleniumcucumber/Jenkinsfile1.groovy
+++ astadia.test.seleniumcucumber/Jenkinsfile1.groovy
@@ -15,7 +15,6 @@ pipeline{
 			   	print "BROWSER-TYPE-VINOD"
 				cleanWs();
 				
-				def scmVars = checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git']]]);
 				
 				checkout([$class: 'GitSCM', branches: [[name: '*/'+ env.ENV_NAME]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git']]])
 				//checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f74659a-05e0-4f91-bd01-ff78f95820e5', url: 'https://github.com/VINODRAMAKRISHNAN/java-ui-api-test-projects.git']]])
